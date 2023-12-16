52f808748d2e1cdeab71a7ac385c98c01ab82c99
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-16 13:20:56 +0300

    initial commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,67 @@
-import { observer } from 'mobx-react-lite';
+import { SearchOutlined } from "@ant-design/icons";
+import { observer } from "mobx-react-lite";
 
 const Home = observer(() => {
-    return <>home</>;
+  return (
+    <>
+      <header>
+        <div className="container header__container">
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__list__item">
+                <a className="item__link" href="">
+                  Главная
+                </a>
+              </li>
+              <li className="nav__list__item">
+                <a className="item__link" href="">
+                  Телепрограмма
+                </a>
+              </li>
+              <li className="nav__list__item">
+                <a className="item__link" href="">
+                  Профиль
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="search">
+            <form className="search__box">
+              <input
+                className="search__box__input"
+                type="text"
+                placeholder="Найдите канал или передачу"
+              />
+              <button className="search__box__icon" type="submit">
+                <SearchOutlined className="search__box__icon_image" />
+              </button>
+            </form>
+          </div>
+        </div>
+      </header>
+      <section className="recs">
+        <div className="container recs__container">
+          <div className="main-rec">
+            <div className="main-rec__info">
+              <h1 className="info__name">Вести, 12+</h1>
+              <p className="info__rate">
+                Рейтинг: 8/10
+                <img
+                  className="rate-icon"
+                  src="images/star-svgrepo-com.svg"
+                  alt="star icon"
+                />
+              </p>
+              <div className="info__classNameify">
+                <span className="info__kategory">Категория</span>
+                <span className="info__genre">Жанр</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 });
 
 export default Home;
