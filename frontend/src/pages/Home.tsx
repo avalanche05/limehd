b8685964c43ef4f66731e067122479bcf208aaf3
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-16 16:06:08 +0300

    add sidenav channels

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { observer } from "mobx-react-lite";
 import Header from "../components/Header";
 import HomeLayout from "../components/HomeLayout";
+import SideChannels from "../components/sideChannels";
 
 const Home = observer(() => {
   return (
     <>
       <Header />
+      <SideChannels />
       <HomeLayout />
     </>
   );
