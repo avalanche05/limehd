3d75ac77c0d9de4d296c101c53cc1891f5f833ee
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-16 14:44:08 +0300

    add other recommendations

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,61 +1,12 @@
-import { SearchOutlined, StarOutlined } from "@ant-design/icons";
 import { observer } from "mobx-react-lite";
+import Header from "../components/Header";
+import HomeLayout from "../components/HomeLayout";
 
 const Home = observer(() => {
   return (
     <>
-      <header>
-        <div className="container header__container">
-          <nav className="nav">
-            <ul className="nav__list">
-              <li className="nav__list__item">
-                <a className="item__link" href="">
-                  Главная
-                </a>
-              </li>
-              <li className="nav__list__item">
-                <a className="item__link" href="">
-                  Телепрограмма
-                </a>
-              </li>
-              <li className="nav__list__item">
-                <a className="item__link" href="">
-                  Профиль
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="search">
-            <form className="search__box">
-              <input
-                className="search__box__input"
-                type="text"
-                placeholder="Найдите канал или передачу"
-              />
-              <button className="search__box__icon" type="submit">
-                <SearchOutlined className="search__box__icon_image" />
-              </button>
-            </form>
-          </div>
-        </div>
-      </header>
-      <section className="recs">
-        <div className="container recs__container">
-          <div className="main-rec">
-            <div className="main-rec__info">
-              <h1 className="info__name">Вести, 12+</h1>
-              <p className="info__rate">
-                Рейтинг: 8/10
-                <StarOutlined className="rate-icon" />
-              </p>
-              <div className="info__classify">
-                <span className="info__kategory">Категория</span>
-                <span className="info__genre">Жанр</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Header />
+      <HomeLayout />
     </>
   );
 });
