679d46243d721a587a804122bf5f39c74d7b3a1b
lisaanthro <liza_vater@yahoo.com>
2023-12-17 00:51:37 +0300

    program serializer update

--- backend/limehd/serializers/program.py
+++ backend/limehd/serializers/program.py
@@ -2,9 +2,10 @@ from sqlalchemy.orm import Session
 
 from limehd.models import program as db_model_program
 from limehd import schemas, serializers, models
+from .stream import get_streams
 
 
-def get_program(db_program: db_model_program.Program) -> schemas.Program:
+def get_program(db_program: db_model_program.Program, user_id: int) -> schemas.Program:
     program = schemas.Program(
         id=db_program.id,
         name=db_program.name,
@@ -15,7 +16,11 @@ def get_program(db_program: db_model_program.Program) -> schemas.Program:
         genre=db_program.genre,
         category=db_program.category,
         image=db_program.image,
-        streams=db_program.streams
+        streams=get_streams(db_program.streams)
     )
 
     return program
+
+
+def get_programs(db_programs: list[db_model_program.Program], user_id: int) -> list[schemas.Program]:
+    return [get_program(db_program, user_id) for db_program in db_programs]
