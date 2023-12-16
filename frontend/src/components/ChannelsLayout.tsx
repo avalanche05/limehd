3c81e55daadbb0505da0d4d8ddb3c7d10cc047c4
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-16 17:56:07 +0300

    fix layout bugs

--- frontend/src/components/ChannelsLayout.tsx
+++ frontend/src/components/ChannelsLayout.tsx
@@ -5,17 +5,19 @@ const ChannelsLayout = observer(() => {
   return (
     <section className="channels">
       <div className="container channels-container">
-        <div className="card-place">
-          <ChannelCard />
-        </div>
-        <div className="card-place card-place">
-          <ChannelCard />
-        </div>
-        <div className="card-place">
-          <ChannelCard />
-        </div>
-        <div className="card-place">
-          <ChannelCard />
+        <div className="row">
+          <div className="card-place">
+            <ChannelCard />
+          </div>
+          <div className="card-place">
+            <ChannelCard />
+          </div>
+          <div className="card-place">
+            <ChannelCard />
+          </div>
+          <div className="card-place">
+            <ChannelCard />
+          </div>
         </div>
       </div>
     </section>
