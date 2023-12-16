90e282c726c7c52164ec64d510b6da7e496127c6
Ilin Ivan <buran05@icloud.com>
2023-12-17 00:43:50 +0300

    version 0.0.2

--- backend/limehd/crud/channel.py
+++ backend/limehd/crud/channel.py
@@ -4,6 +4,7 @@ from sqlalchemy import func, and_
 from sqlalchemy.orm import Session
 from limehd import models, schemas
 from datetime import datetime
+from Levenshtein import distance
 
 
 def get_channel_by_channel_id(db: Session, id: int) -> models.Channel:
@@ -12,16 +13,36 @@ def get_channel_by_channel_id(db: Session, id: int) -> models.Channel:
     return channel
 
 
+def levenshtein_search(string1: str, string2: str, threshold: int = 3) -> bool:
+    return distance(string1, string2) <= threshold
+
+
 def get_channels(
         db: Session,
         search_name: str = None,
-):
+        start: datetime = None,
+        finish: datetime = None,
+) -> List[models.Channel]:
     query = db.query(models.Channel)
 
     if search_name:
-        query = query.filter(models.Channel.name.ilike(f"%{search_name}%"))
+        channels = [channel for channel in channels if levenshtein_search(channel.name, search_name)]
+
+    if start and finish:
+        channel_ids = (
+            db.query(models.Channel.id)
+            .join(models.Channel.streams)
+            .filter(models.Stream.start >= start, models.Stream.finish <= finish)
+            .distinct()
+            .all()
+        )
+
+        channel_ids = [channel_id for (channel_id,) in channel_ids]
+
+        query = query.filter(models.Channel.id.in_(channel_ids))
 
     channels = query.all()
+
     return channels
 
 
