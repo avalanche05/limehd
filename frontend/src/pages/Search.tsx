2012101328be7e83f08d01944aa2919af2cca37a
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-12-16 20:38:33 +0300

    create search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,6 +1,46 @@
 import { observer } from "mobx-react-lite";
 import ProgrammsLayout from "../components/ProgrammsLayout";
-import ChannelsLayout from "../components/ChannelsLayout";
+
+const ChannelsSimplifyCard = observer(() => {
+  return (
+    <>
+      <div className="channel-simplify-card">
+        <div className="channel-image">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/MatchTV_Logo.svg/1200px-MatchTV_Logo.svg.png"
+            alt="Channel logo"
+          />
+        </div>
+        <div className="channel-info">
+          <span className="name">Матч!</span>
+          <p className="desc">Краткое описание</p>
+        </div>
+      </div>
+    </>
+  );
+});
+
+const ChannelsSimplifyLayout = observer(() => {
+  return (
+    <>
+      <div className="row">
+        <div className="card-place">
+          <ChannelsSimplifyCard />
+        </div>
+        <div className="card-place">
+          <ChannelsSimplifyCard />
+        </div>
+        <div className="card-place">
+          <ChannelsSimplifyCard />
+        </div>
+        <div className="card-place">
+          <ChannelsSimplifyCard />
+        </div>
+      </div>
+    </>
+  );
+});
+
 const Search = observer(() => {
   return (
     <>
@@ -15,7 +55,7 @@ const Search = observer(() => {
           </div>
           <div className="channel-response">
             <div className="title category-title">Каналы</div>
-            <ChannelsLayout />
+            <ChannelsSimplifyLayout />
           </div>
         </div>
       </section>
