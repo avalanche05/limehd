ce73204602726b56159997c04d9acc2de25dce72
maj0rio <fadeevvanya@gmail.com>
2023-12-17 09:40:58 +0300

    Fix serialize

--- backend/limehd/serializers/user.py
+++ backend/limehd/serializers/user.py
@@ -11,8 +11,8 @@ def get_user(db_user: db_model_user.User) -> schemas.User:
         email=db_user.email,
         hashed_password=db_user.hashed_password,
         fingerprint=db_user.fingerprint,
-        favorite_programs=db_user.programs,
-        favorite_channels=db_user.channels
+        favorite_programs=[serializers.get_program(program) for program in db_user.programs],
+        favorite_channels=[serializers.get_channel(channel) for channel in db_user.channels]
     )
 
     return user
