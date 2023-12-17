26b4f385e82b68d820974870f53f88cc97da4182
maj0rio <fadeevvanya@gmail.com>
2023-12-17 05:04:03 +0300

    like program

--- backend/limehd/routers/program.py
+++ backend/limehd/routers/program.py
@@ -26,10 +26,15 @@ def get_program_by_program_id(id: int, db: Session = Depends(get_db)) -> schemas
 
 
 @program_router.post(path="/{id}/rating")
-def add_program_rating(id: int, db: Session = Depends(get_db)):
-    ...
+def add_program_rating(response: Response, id: int, user: models.User = Depends(current_user), db: Session = Depends(get_db)):
+    cookie = user.fingerprint
+    response.set_cookie(key='fingerprint', value=cookie)
 
 
 @program_router.post(path="/{id}/like")
-def like_program(id: int, db: Session = Depends(get_db)):
-    ...
+def like_program(response: Response, id: int, user: models.User = Depends(current_user), db: Session = Depends(get_db)):
+    cookie = user.fingerprint
+    response.set_cookie(key='fingerprint', value=cookie)
+    program = crud.program.get_program_by_program_id(id)
+    new_user = crud.users.add_program(db, user, program)
+    return new_user
